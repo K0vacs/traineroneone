fd8ffe86334790c0c53a2728b9a9703377361929
Ferenc Kovacs <mr.f.kovacs@gmail.com>
2019-05-21 11:46:02 +0000

    Testing connection from GitHub to Heroku

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello New World!"
     
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), 
