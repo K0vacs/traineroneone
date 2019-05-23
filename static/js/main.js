87ba53730cc035ed22133d3c917dc9aef2bffd86
Ferenc Kovacs <mr.f.kovacs@gmail.com>
2019-05-23 04:39:41 +0000

    static file added with main.js file

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    
+  $(".button-collapse").sideNav();
+    
+  $('.carousel').slick({
+    accessibility: true,
+    autoplay: true,
+    arrows: true,
+    autoplaySpeed: 5000,
+    dots: true
+  });
+  
+});
