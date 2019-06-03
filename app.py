2c4bfb55614b5c58d4e95d40a9be2d76bf94ac2d
Ferenc Kovacs <mr.f.kovacs@gmail.com>
2019-06-03 13:05:12 +0000

    Basic form validation added and MongoDB _id returned after insert

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ def add_program():
 def test():
     if request.method == 'POST':
         varss = request.form['exercise-name']
-        return varss
+        result = mongo.db.TOOCollection.insert_one({"name": varss})
+        return str(result.inserted_id)
 
 
 if __name__ == "__main__":
--- static/js/main.js
+++ static/js/main.js
@@ -20,26 +20,52 @@ $(document).ready(function() {
   
   $('select').material_select();
   
-  
-  $("[data-save='excersise']").on("click", function(event) {
+  $("[data-save='exercise']").on("click", function(event) {
     event.preventDefault();
     var exerciseName = $("[name='exercise-name']").val();
-    showExercise(exerciseName);
-    sessionVariable(exerciseName);
+    validateForm(exerciseName);
+  });
+  
+  function validateForm(formData) {
+    if(formData == "") {
+      $("#exercise-name").after("<span class='error-message'>Field cannot be blank!");
+    } else {
+      AjaxRequest();
+      showExercise(formData);
+      sessionVariable(formData);
+      $(".error-message").remove();
+      $("#exercise-form").trigger("reset");
+    }
+  }
+  
+  $("[name='exercise-name']").on("mouseenter", function(event) {
+    $("[data-save='exercise'] i").text("save");
+  });
+  
+  function AjaxRequest() {
     
+    $("[data-save='exercise'] i").text("refresh").addClass("spin-icon");
+    $("[data-save='exercise']").attr("disabled", true);
+
     $.ajax({
 			url: '/test',
 			data: $('#exercise-form').serialize(),
 			type: 'POST',
 			success: function(response){
+			  $("[data-save='exercise'] i").text("done").removeClass("spin-icon")
+			  
+			 // setTimeout(function(){ 
+			    
+			 // }, 3000);
+			  
+			  $("[data-save='exercise']").attr("disabled", false);
 				console.log(response);
 			},
 			error: function(error){
 				console.log(error);
 			}
 		});
-		
-  });
+  }
   
   function sessionVariable(form) {
     var list = {
