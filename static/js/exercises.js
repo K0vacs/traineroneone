e45ce830bedee4af6f7fa2be71bc1b3a2e1139ef
Ubuntu <ubuntu@ip-172-31-17-116.eu-west-1.compute.internal>
2019-08-07 17:00:12 +0000

    Jinja templates broken into smaller templates.

---
+++ static/js/exercises.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  
+    // Global variables and Materialize prototypes executed on load -------------- //
+    $('.sidenav').sidenav();
+    
+    // Slick carousels initialized ----------------------------------------------- //
+    $( ".exercises-carousel" ).show().slick({
+    dots: true,
+    arrows: false
+  });
+  
+    // jQuery events ------------------------------------------------------------- //
+    
+    // This click event opens the side panel on mobile.
+    $( ".sidenav-trigger" ).on( "click", function() {
+    $( ".popout-panel" ).addClass( "popout-open" );
+    });
+    
+    // This click event closes the side panel on mobile.
+    $( ".sidenav-close" ).on( "click", function() {
+    $( ".popout-panel" ).removeClass( "popout-open" );
+    });
+    
+});
